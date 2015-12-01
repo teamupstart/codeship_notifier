cf9a297a7a1a4bc3256109c8c2b422d3b54d0c33
Andrew Bezzub <andrew@bezzub.com>
2015-11-30 17:37:13 -0800

    Make sure we know about build error

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -6,11 +6,12 @@ class GithubWebhooksController < ApplicationController
 
     if action.eql?("opened") || action.eql?("reopened") || action.eql?("synchronize")
       commit = payload["pull_request"]["head"]["sha"]
+
       build = Build.find_by_commit(commit) || Build.new
       build.commit = commit
       build.head_repository = payload["pull_request"]["head"]["repo"]["full_name"]
       build.base_repository = payload["pull_request"]["base"]["repo"]["full_name"]
-      build.save
+      build.save!
 
       create_status_notification_if_build_exists(build)
     end
